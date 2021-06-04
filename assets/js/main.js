ff544317fc95a4376bea566717f675e2cf12b0d3
myplancash <myplancash@gmail.com>
2021-06-04 16:33:19 -0500

    new blog design

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,6 +260,29 @@ $(document).ready(function($) {
     }
   };
 
+	 /* Back to Top
+    * ------------------------------------------------------ */
+    const ssBackToTop = function() {
+
+        const pxShow = 900;
+        const goTopButton = document.querySelector(".ss-go-top");
+
+        if (!goTopButton) return;
+
+        // Show or hide the button
+        if (window.scrollY >= pxShow) goTopButton.classList.add("link-is-visible");
+
+        window.addEventListener('scroll', function() {
+            if (window.scrollY >= pxShow) {
+                if(!goTopButton.classList.contains('link-is-visible')) goTopButton.classList.add("link-is-visible")
+            } else {
+                goTopButton.classList.remove("link-is-visible")
+            }
+        });
+
+    }; // end ssBackToTop
+
+
 
 
 
