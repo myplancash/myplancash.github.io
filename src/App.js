e86e4dd6feb0edf5fe7b68f9c912ef8e0a75eba4
Sergio Torres <sergiotorres@Sergios-MacBook-Pro-M2.local>
2023-10-11 10:28:20 -0500

    lazy loading optimizing react app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container, MainContent } from './App.styles'
-import AboutMe from './components/AboutMe/AboutMe';
-import Projects from './components/Projects/Projects';
-import Skills from './components/Skills/Skills';
-import Contact from './components/Contact/Contact';
-
-import HeroBanner from './components/HeroBanner/HeroBanner';
-import Blog from './components/Blog/Blog';
 import blogData from './data/blogData';
-import BlogPostDetail from './components/BlogPostDetail/BlogPostDetail';
-import Footer from './components/Footer/Footer';
+
+
+const LazyComponent = (importFunc) => {
+  const Component = lazy(importFunc);
+  return (props) => (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component {...props} />
+    </Suspense>
+  )
+}
+
+const AboutMe = LazyComponent(() => import('./components/AboutMe/AboutMe'));
+const Projects = LazyComponent(() => import('./components/Projects/Projects'));
+const Skills = LazyComponent(() => import('./components/Skills/Skills'));
+const Contact = LazyComponent(() => import('./components/Contact/Contact'));
+const HeroBanner = LazyComponent(() => import('./components/HeroBanner/HeroBanner'));
+const Blog = LazyComponent(() => import('./components/Blog/Blog'));
+const BlogPostDetail = LazyComponent(() => import('./components/BlogPostDetail/BlogPostDetail'));
+const Footer = LazyComponent(() => import('./components/Footer/Footer'));
+
 
 const App = () => {
   return (
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,12 +5,12 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    font-family: 'Lato', sans-serif;
+    font-family: 'Roboto', sans-serif;
     font-weight: 400;
   }
 
   h1, h2, h3, h4, h5, h6 {
-    font-family: 'Raleway', sans-serif;
+    font-family: 'Poppins', sans-serif;
     font-weight: 700;
     font-size: 2rem;
   }
